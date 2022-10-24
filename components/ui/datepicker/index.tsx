ae1e462bbed0da48586801b969667b2ad9888bfe
default <ahmet1ozbey@gmail.com>
2022-10-24 08:00:09 +0300

    test: deployment test-7

--- components/ui/datepicker/index.tsx
+++ components/ui/datepicker/index.tsx
@@ -1,24 +1,12 @@
 import * as React from 'react';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import styles from './datepicker.module.css'
-import { CalendarPicker } from '@mui/x-date-pickers';
 
 export interface IAppProps {
-  date:Date;
-  setDate:Function;
 }
 
 export default function App (props: IAppProps) {
-    const [date, setDate] = React.useState(props.date);
-    const [value, setValue] = React.useState(new Date());
-    const handleChange = (newValue:Date) => {
-      setValue(newValue);
-    };
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-    <CalendarPicker date={props.date} onChange={(newDate:any) => props.setDate(newDate)}  className={styles['datepicker']}/>
-      
-</LocalizationProvider>
+    <div>
+      div
+    </div>
   );
 }
