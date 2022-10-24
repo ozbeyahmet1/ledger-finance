961ca575438fe9ae742e1c2aa937b7d4418a4732
default <ahmet1ozbey@gmail.com>
2022-10-24 07:22:30 +0300

    Revert "test: deployment test-1"
    This reverts commit 4a313a6b70ea23f20b5bc4c5303d730a0f6e84a1.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
+
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'res.cloudinary.com',
-    ],
-},
-eslint: {
-  ignoreDuringBuilds: true,
-},
-future: { 
-  webpack5: true 
-}
-}
+    reactStrictMode: true,
+    images: {
+      domains: [
+        'res.cloudinary.com',
+      ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  future: { 
+    webpack5: true 
+  }
+  }
